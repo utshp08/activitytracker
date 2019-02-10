bf2a976164f62d5d4283c504f09dc8bbfd8638b6
Reymart Pantonia <utspantonia@gmail.com>
2019-02-11 04:09:09 +0800

    fix error

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === "production") {
     module.exports = {
-        mongoURI : "mongodb+srv://utspantonia:secret123@cluster0-shard-00-01-oguaj.mongodb.net:27017/activitytracker?ssl=true"
+        mongoURI : "mongodb+srv://utspantonia:secret123@cluster0-oguaj.mongodb.net/activitytracker?retryWrites=true&ssl=true"
     }
 } else {
     module.exports = {
