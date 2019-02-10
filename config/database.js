dd59ccfc562ef4dcf1e9a0f7b6bc2eb3e9b6e314
Reymart Pantonia <utspantonia@gmail.com>
2019-02-11 05:15:06 +0800

    fix errors

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === "production") {
     module.exports = {
-        mongoURI : "mongodb+srv://utspantonia:secret123@cluster0-oguaj.mongodb.net/activitytracker?retryWrites=true&ssl=true"
+        mongoURI : "mongodb://activitytracker-prod-shard-00-00-0pf4a.mongodb.net:27017,activitytracker-prod-shard-00-01-0pf4a.mongodb.net:27017,activitytracker-prod-shard-00-02-0pf4a.mongodb.net:27017/test?replicaSet=activitytracker-prod-shard-0 --ssl --authenticationDatabase admin --username utspantonia --password secret123"
     }
 } else {
     module.exports = {
