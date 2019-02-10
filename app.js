3c305fbce4f72f8c16d2ac402013d5103c0a39d1
Reymart Pantonia <utspantonia@gmail.com>
2019-02-11 03:43:21 +0800

    fix error

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log('Server started at localhost:5000');
+    console.log('Server started at port:' + port);
 });
 
 //Load database
