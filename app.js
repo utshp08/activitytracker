31a1c5ea815fbad08a2324f7f14150b51050ff46
Reymart Pantonia <utspantonia@gmail.com>
2019-02-11 04:28:09 +0800

    fix error

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.listen(port, () => {
 
 //Setting up mongoose database
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://utspantonia:secret123@cluster0-oguaj.mongodb.net/activitytracker?retryWrites=true&ssl=true", {
+mongoose.connect("mongodb+srv://utspantonia:@cluster0-oguaj.mongodb.net/activitytracker?retryWrites=true&ssl=true", {
     useNewUrlParser: true
 }).then(() => {
     console.log('Database connection established.');
