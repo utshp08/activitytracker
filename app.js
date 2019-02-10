aaf265c14b9d1ef2a2120d884a751e338046f8a3
Reymart Pantonia <utspantonia@gmail.com>
2019-02-11 06:05:34 +0800

    fix errors

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.listen(port, () => {
 
 //Setting up mongoose database
 mongoose.Promise = global.Promise;
-mongoose.connect(db.mongoURI, {dbName: "activitytracker", useNewUrlParser: true})
+mongoose.connect(db.mongoURI, {dbName: "activitytracker", useNewUrlParser: true, useMongoClient: true})
 .then(() => {
     console.log('Database connection established.');
 }).catch(err => {
