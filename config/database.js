319987b25a093a52609c06ea64f1812c4e1c1433
utspantonia <utspantonia@gmail.com>
2019-03-25 14:46:47 +0800

    ignore db

--- config/database.js
+++
@@ -1,9 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-    module.exports = {
-        mongoURI : "mongodb+srv://utspantonia:secret123@cluster0-oguaj.mongodb.net/activity_tracker?retryWrites=true"
-    }
-} else {
-    module.exports = {
-        mongoURI : "mongodb://localhost/activitytracker-dev"
-    }
-}
